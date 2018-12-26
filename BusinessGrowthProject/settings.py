2ca141c85bcae61b91a9aa6e0fb880bb125b7930
Marvel.G <mgomulya@gmail.com>
2018-12-26 10:51:40 +0700

    Added gzip middleware

--- BusinessGrowthProject/settings.py
+++ BusinessGrowthProject/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware'
 ]
 
 ROOT_URLCONF = 'BusinessGrowthProject.urls'
